ed24a9195894926d786950591e395ff9ad8e8b5f
Kasia <kasia@wiseman.pl>
2016-12-30 21:45:36 +0100

    drugi commit

--- src/ZadanieTriangle/TestTriangle.java
+++ src/ZadanieTriangle/TestTriangle.java
@@ -1,11 +1,21 @@
 package ZadanieTriangle;
 
 /**
- * Created by Kasia on 29.12.2016.
+ * Created by Kasia on 30.12.2016.
  */
 public class TestTriangle {
     public static void main(String[] args) {
-        Triangle triangle = new Triangle();
+        Triangle triangle=new Triangle(90);
+        System.out.println(triangle.Triangle());
+        triangle.getAngle();
+        triangle.setAngle(90);
 
+        public	boolean	isRightTriangle(Triangle triangle){
+        if(triangle.getAngle()==90){
+            System.out.println("true");
+        }else {
+            System.out.println("false");
+        }
     }
-}
+    }
+
--- src/ZadanieTriangle/Triangle.java
+++ src/ZadanieTriangle/Triangle.java
@@ -4,9 +4,44 @@ package ZadanieTriangle;
  * Created by Kasia on 29.12.2016.
  */
 public class Triangle {
-    double a;
-    double b;
-    double c;
-    double h;
-    double angle;
+    private double a;
+    private double b;
+    private double c;
+    private double h;
+    private double angle;
+
+
+    public double getAngle() {
+        return angle;
+    }
+
+    public void setAngle(double angle) {
+        this.angle = angle;
+    }
+
+    public Triangle(double angle){
+        this.setAngle(angle);
+    }
+
+    public Triangle(Triangle triangle){
+        this(triangle.getAngle());
+    }
+
+    public void Triangle(Triangle triangle){//ustawiam wartość kąta 90, dla którego warunek będzie true
+        if(angle==90){
+            this.angle=angle;
+            System.out.println("true");
+        }else{
+            System.out.println("false");
+        }
+    }
 }
+
+
+
+/*
+Stwórz	klasę	Triangle	reprezentującą	trójkąt.	Następnie	w	osobnej	klasie	zdefiniuj	metodę:
+public	boolean	isRightTriangle(Triangle	triangle){	…	}
+która	zwraca	true	jeśli	trójkąt	przekazany	jako	argument	jest	prostokątny	lub	false
+jeśli	nie	jest.	Przetestuj	działanie	metody	w	osobnej	klasie.
+ */
